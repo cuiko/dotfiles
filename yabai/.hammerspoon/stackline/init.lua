2add14f13b46d0d1e7eaa317ae63dc2a304b8619
cuiko <1064291677@qq.com>
2023-02-01 14:23:11 +0800

    break(yabai): remove yabai config to single repo

--- yabai/.hammerspoon/stackline/init.lua
+++
@@ -1,4 +0,0 @@
--- Simplify requiring stackline from ~/hammerspoon/init.lua
-
-package.path = os.getenv'HOME' ..'/.hammerspoon/stackline/?.lua;' .. package.path
-return require 'stackline.stackline'
