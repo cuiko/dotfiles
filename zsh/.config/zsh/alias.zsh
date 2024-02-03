d834fbfcafde684a737cbb7f10eb48f01c24421c
cuiko <cuiko.tang@gmail.com>
2024-02-03 16:35:03 +0800

    break: optimize zsh config

--- zsh/.config/zsh/alias.zsh
+++ zsh/.config/zsh/alias.zsh
@@ -1,4 +1,3 @@
-# alias
 command_check() {
 	command -v $1 &>/dev/null
 }
@@ -12,51 +11,3 @@ alias cp='cp -v'   mv='mv -v'
 
 alias python='python3'
 alias pip='pip3'
-
-# functions
-proxy() {
-  if [[ "$1" != "" ]]; then
-    local proxy_addr="$1"
-  fi
-  export {http,https}_proxy="${proxy_addr:-http://localhost:"${proxy_port:-7890}"}"
-  echo "proxy on: $http_proxy"
-}
-
-unproxy() {
-  unset {http,https}_proxy
-  echo "proxy off"
-}
-
-is_proxy() {
-  if [[ "$http_proxy" == "" || "$https_proxy" == "" ]]; then
-    return 1
-  fi
-}
-
-ap() {
-  case $1 in
-    status | s)
-      if is_proxy; then
-        echo "status: on, proxy address $http_proxy"
-      else
-        echo "status: off"
-      fi
-      ;; 
-
-    on)
-      proxy "$2"
-      ;;
-
-    off)
-      unproxy
-      ;;
-
-    "")
-      is_proxy && unproxy || proxy
-      ;;
-
-    *)
-      echo "error: unknown arguments"
-      ;;
-  esac
-}
--- zsh/.zshenv
+++ zsh/.zshenv
@@ -2,8 +2,31 @@ export XDG_CONFIG_HOME=$HOME/.config
 export XDG_CACHE_HOME=$HOME/.cache
 export XDG_DATA_HOME=$HOME/.local/share
 
-export LANG="en_US.UTF-8"
+export TERM=${TERM:-xterm-256color}
+export LANG=${LANG:-"en_US.UTF-8"}
+
+command -v nvim >/dev/null && export EDITOR=nvim || export EDITOR=vim
 
 ZDOTDIR=$XDG_CONFIG_HOME/zsh
+export ENVFILEPATH=$ZDOTDIR/env
+
 skip_global_compinit=1
 
+# starship
+export STARSHIP_CONFIG=$ZDOTDIR/starship.toml
+
+# ssh
+export SSH_PROXY=127.0.0.1:1081
+
+# go
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export GOBIN=$GOPATH/bin
+export GO111MODULE=on
+export GOPROXY=https://goproxy.io,direct
+
+# nvm
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+nvml() {
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+}
